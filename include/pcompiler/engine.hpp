ff9a91b96bf8bc96329561ae331662d7557c8a4b
kleiinnn <m@mklein.co.at>
2014-01-03 23:16:06 +0100

    removed default arguments for options

--- include/pcompiler/engine.hpp
+++ include/pcompiler/engine.hpp
@@ -25,7 +25,7 @@ namespace Compiler
 		void addCompilers(const QList<const Base *>& compilers);
 		void removeCompiler(const Base *compiler);
 		
-		OutputList compile(const Input& input, const kiss::KarPtr& program = kiss::Kar::create(), Options& options = Options(), Progress *progress = 0) const;
+		OutputList compile(const Input& input, const kiss::KarPtr& program = kiss::Kar::create(), Options& options, Progress *progress = 0) const;
 		
 	private:
 		QList<const Base *> m_compilers;
