da1bb0df96e0f8596fb3532851ea0d65682cb055
kleiinnn <m@mklein.co.at>
2014-01-03 14:14:32 +0100

    removed default parameter from compilers and fixed default parameter in engine

--- include/pcompiler/base.hpp
+++ include/pcompiler/base.hpp
@@ -17,7 +17,7 @@ namespace Compiler
 	public:
 		Base(const QString& name, const QStringList& extensions, const int& precedence, const QStringList& flags = QStringList());
 		
-		virtual OutputList transform(const QStringList& input, const Options& options, const kiss::KarPtr& program = 0) const = 0;
+		virtual OutputList transform(const QStringList& input, const Options& options, const kiss::KarPtr& program) const = 0;
 		
 		const QString& name() const;
 		const QStringList& extensions() const;
